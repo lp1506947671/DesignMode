3e704b1539b3d0e87e2099772fc7691fc2e86656
lp1506947671 <1506946771@qq.com>
2020-12-24 08:30:17 +0800

    [类型]:需求开发 [描述]:桥接模式,组合模式 [提交人]:Jason

---
+++ 6.桥接模式.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from abc import ABCMeta, abstractmethod
+
+
+# 抽象者
+class Shape(metaclass=ABCMeta):
+    def __init__(self, color):
+        self.color = color
+
+    @abstractmethod
+    def draw(self):
+        pass
+
+
+# 细化抽象
+class Rectangle(Shape):
+    name = "长方形"
+
+    def draw(self):
+        self.color.paint(self)
+
+
+class Circle(Shape):
+    name = "圆形"
+
+    def draw(self):
+        self.color.paint(self)
+
+
+# 实现者
+class Color(metaclass=ABCMeta):
+    @abstractmethod
+    def paint(self, shape):
+        pass
+
+
+# 具体实现者
+
+class Red(Color):
+    def paint(self, shape):
+        print("红色的%s" % shape.name)
+
+
+class Black(Color):
+    def paint(self, shape):
+        print("黑色的%s" % shape.name)
+
+
+if __name__ == '__main__':
+    shape = Rectangle(Red())
+    shape.draw()
