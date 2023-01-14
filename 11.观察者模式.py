47afbb2bf95c35a4f0a4569782ae02a600ca7e9e
lp1506947671 <xiaopawnye@163.com>
2023-01-14 15:51:07 +0800

    [类型]:需求开发 [描述]:观察设计模式优化 [提交人]:Jason

--- 11.观察者模式.py
+++ 11.观察者模式.py
@@ -5,81 +5,90 @@ from abc import ABCMeta, abstractmethod
 
 # 抽象观察者
 class Observer(metaclass=ABCMeta):
+
     @abstractmethod
     def update(self, notice):
-        pass
+        ...
 
 
 # 抽象发布者
-class Notice:
-    """集成对抽象观察者的更新"""
+class Notice(metaclass=ABCMeta):
 
     def __init__(self):
-        self.observers = []
+        self.staff_list = []
 
-    def attach(self, obs):
-        self.observers.append(obs)
+    def attach(self, staff):
+        self.staff_list.append(staff)
 
-    def detach(self, obs):
-        self.observers.remove(obs)
+    def detach(self, staff):
+        self.staff_list.remove(staff)
+        staff.company_info = None
 
     def notify(self):
-        for obs in self.observers:
-            obs.update(self)
+        for item in self.staff_list:
+            item.update(self)  # 注意点1:item和self
 
 
-# 具体发布者
-class StaffNotice(Notice):
+# 抽象发布者
+class Company(Notice):
     """
-    1.继承Notice通知类,实现对那些需要通知的员工进行增删改查
-    2.定义自己需要发布的信息
-    3.每设置一次信息就需要通知一次所有的在公司的员工
-    """
+      1.继承Notice通知类,实现对那些需要通知的员工进行增删改查
+      2.定义自己需要发布的信息
+      3.每设置一次信息就需要通知一次所有的在公司的员工
+      """
 
     def __init__(self, company_info=None):
-        super().__init__()
-        # 定义要发布的信息
+        super(Company, self).__init__()
         self.__company_info = company_info
 
     @property
     def company_info(self):
         return self.__company_info
 
-    @company_info.setter
-    def company_info(self, info):
-        """每设置一次公司消息就要通知员工"""
-        self.__company_info = info
-        # 通知每一个公司员工
+    @company_info.setter  # 注意点2
+    def company_info(self, data):
+        self.__company_info = data
         self.notify()
 
 
 # 具体观察者
-class Staff(Observer):
+class ChineseStaff(Observer):
+
     def __init__(self):
-        # 定义要接收的信息
         self.company_info = None
 
-    def update(self, notice):
-        # 更新公司消息
+    def update(self, notice: Company):
         self.company_info = notice.company_info
 
 
+class ForeignStaff(Observer):
+    def __init__(self):
+        self.company_info = None
+
+    def update(self, notice: Company):
+        self.company_info = f"翻译为英文:{notice.company_info}"
+
+
 if __name__ == '__main__':
-    n1 = StaffNotice("")
-    s1 = Staff()
-    s2 = Staff()
-    n1.attach(s1)
-    n1.attach(s2)
+    staff1 = ChineseStaff()
+    staff2 = ChineseStaff()
+    staff3 = ForeignStaff()
+
+    company = Company()
+    company.attach(staff1)
+    company.attach(staff2)
+    company.attach(staff3)
     print("begin")
-    print(s1.company_info)
-    print(s2.company_info)
+    print(staff1.company_info)
+    print(staff2.company_info)
+    print(staff3.company_info)
     print("end")
-    print("公司通知大家")
-    n1.company_info = "公司今年业绩非常好,给大家发奖金"
-    print(s1.company_info)
-    print(s2.company_info)
-    print("删除员工s1")
-    n1.detach(s1)
-    n1.company_info = "明天放假"
-    print(s1.company_info)
-    print(s2.company_info)
+    company.company_info = "公司明天放假"
+    print(staff1.company_info)
+    print(staff2.company_info)
+    print(staff3.company_info)
+    company.detach(staff1)
+    company.company_info = "公司明天发工资"
+    print(staff1.company_info)
+    print(staff2.company_info)
+    print(staff3.company_info)
